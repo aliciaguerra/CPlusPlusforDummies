40a7b581fd82cee99dcc2e35a5ca077a91d134d4
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-22 01:33:52 -0700

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,13 @@
+#include <cstudio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int nNumberorArgs, char* pszArgs[])
+{
+ //your c++ code starts here
+ cout<< "Press enter to continue" <<end1;
+ cin.ignore(10, '\n');
+ cin.get();
+ return 0;
+ }
